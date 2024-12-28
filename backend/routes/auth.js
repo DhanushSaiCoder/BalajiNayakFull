e3eb044515f2dc65caa80d58937c75ba713cad8d
DhanushSaiCoder <dhanushsai1467@gmail.com>
2024-12-29 02:39:45 +0530

    token expiration

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,7 +57,7 @@ router.post("/login", async (req, res) => {
     }
 
     // Generate JWT token if credentials are correct
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1m' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
     // Send the token to the client to store in localStorage
     res.json({ message: "Login successful", token });
