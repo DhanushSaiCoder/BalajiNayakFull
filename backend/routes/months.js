14253a8ccf7fc96e2b3f0fe1697b342a2ea0e602
DhanushSaiCoder <dhanushsai1467@gmail.com>
2024-12-28 22:53:20 +0530

    no duplicate month documents allowed from now

--- backend/routes/months.js
+++ backend/routes/months.js
@@ -11,28 +11,45 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-    console.log('Received POST request with body:', req.body);
+    try {
 
-    const { error } = validateMonth(req.body);
-    if (error) {
-        console.log('Validation error:', error.details[0].message);
-        return res.status(400).send(error.details[0].message);
+        console.log('Received POST request with body:', req.body);
+
+        const existingMonth = await Month.findOne({ month: req.body.month, year: req.body.year });
+
+        if (existingMonth) {
+            console.log('Month already exists:', existingMonth);
+            return res.status(400).send({
+                short: "duplicateMonth",
+                message: 'Month already exists for the given year.'
+            });
+        }
+
+
+        const { error } = validateMonth(req.body);
+        if (error) {
+            console.log('Validation error:', error.details[0].message);
+            return res.status(400).send({
+                short: "invalidMonth",
+                message: error.details[0].message
+            });
+        }
+
+
+        const month = new Month({
+            month: req.body.month,
+            monthName: req.body.monthName,
+            year: req.body.year,
+            days: req.body.days
+        });
+
+        console.log('Saving month:', month);
+        await month.save();
+        res.send(month);
+    } catch (ex) {
+        console.log('Error:', ex.message);
+        res.status(500).send('Something failed.');
     }
-
-    const month = new Month({
-        month: req.body.month,
-        monthName: req.body.monthName,
-        year: req.body.year,
-        days: req.body.days
-    });
-
-    console.log('Saving month:', month);
-    await month.save();
-    console.log('Month saved successfully:', month);
-
-    res.send(month);
 });
-
-
 
 module.exports = router;
