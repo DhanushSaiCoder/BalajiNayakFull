c6b6b2e825c2f5aedefcf101802db7e94ca95cd0
DhanushSaiCoder <dhanushsai1467@gmail.com>
2024-12-29 18:35:48 +0530

    +nav

--- frontend/balaji-nayak-client/src/components/Home.js
+++ frontend/balaji-nayak-client/src/components/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import '../home.css'
 function Home() {
   if (!localStorage.getItem('BNtoken')) {
     window.location.href = '/login'
   }
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <div className='leftNav'>
+        <div className='navHeaderDiv'>
+          <h1>ATTENDANCE TRACKER</h1>
+        </div>
+        <div className='navContentDiv'>
+          <h1>content</h1>
+        </div>
+        <div className='navFooterDiv'>
+          <h1>footer</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
