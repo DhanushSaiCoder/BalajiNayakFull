29e0f6b3d49edbcfd8b4183996214ae7e471c891
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-01-07 19:59:57 +0530

    +logOutBtn

--- frontend/balaji-nayak-client/src/components/MobileNav.js
+++ frontend/balaji-nayak-client/src/components/MobileNav.js
@@ -4,17 +4,28 @@ import '../home.css';
 const MobileNav = ({ currPage, handlePageChange }) => {
     return (
         <div className="mobileNav">
-            <button 
-                onClick={() => handlePageChange('enterAttendance')} 
-                className={currPage === 'enterAttendance' ? 'navButtons activeNavBtn' : 'navButtons'}
+            <button
+                onClick={() => handlePageChange('enterAttendance')}
+                className={currPage === 'enterAttendance' ? 'mobileNavButtons activeNavBtn' : 'mobileNavButtons'}
             >
                 Enter Attendance
             </button>
-            <button 
-                onClick={() => handlePageChange('reports')} 
-                className={currPage === 'reports' ? 'navButtons activeNavBtn' : 'navButtons'}
+            <button
+                onClick={() => handlePageChange('reports')}
+                className={currPage === 'reports' ? 'mobileNavButtons activeNavBtn' : 'mobileNavButtons'}
             >
                 Reports
+            </button>
+            <button
+                onClick={() => {
+                    localStorage.removeItem('BNtoken')
+                    window.location.href = '/login'
+                }}
+                // className={currPage === 'reports' ? 'mobileNavButtons activeNavBtn' : 'mobileNavButtons'}
+                className='mobileNavButtons'
+                id='mobileLogOutBtn'
+            >
+                Log Out
             </button>
         </div>
     );
